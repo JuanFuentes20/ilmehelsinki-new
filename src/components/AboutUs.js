d2282607a4045bda58172f4f5db1aaadb735eefe
Juan Fuentes <59918928+JuanFuentes20@users.noreply.github.com>
2023-12-19 22:57:55 +0200

    Style changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,17 +2,17 @@ export default function AboutUs() {
 
     return (
         <div className="about-us">
-            <div className="about-hero">
+            <header className="about-hero">
                 <img className="about-us-image" alt="Kanto" src={process.env.PUBLIC_URL + "/images/kanto.jpeg"}/>
                 <p>Käsintehtyjen huonekalujen valmistus luo ainutlaatuisen yhteyden työhön. Se herättää arvostusta käytettyjä materiaaleja ja valmistusprosessia kohtaan, muuttaen jokaisen huonekalun kotisi henkilökohtaiseksi tarinaksi.</p>
-            </div>
+            </header>
             <div className="about-main">
-                <img className="collab-image reveal" alt="Yhteiskuva" src={process.env.PUBLIC_URL + "/images/yhteiskuva.jpeg"}/>
-                <p className="reveal">Ilme on Helsinkiläinen design studio, joka tuottaa ainutlaatuisia huonekaluja inspiroiviin kohtauspaikkoihin kotona, toimistoissa ja julkisissa tiloissa. Omaperäiset ja rohkeat muodot näkyvät tuotteissamme. Valmistamme huonekaluja sekä taide-esineitä Ilmeen omalla verstaalla Helsingissä. Toteutamme myös mittatilaustöitä ja suurempia kokonaisuuksia. Näistä lisää alempana kohdassa projektit.</p>
+                <img className="collab-image" alt="Yhteiskuva" src={process.env.PUBLIC_URL + "/images/yhteiskuva.jpeg"}/>
+                <p>Ilme on Helsinkiläinen design studio, joka tuottaa ainutlaatuisia huonekaluja inspiroiviin kohtauspaikkoihin kotona, toimistoissa ja julkisissa tiloissa. Omaperäiset ja rohkeat muodot näkyvät tuotteissamme. Valmistamme huonekaluja sekä taide-esineitä Ilmeen omalla verstaalla Helsingissä. Toteutamme myös mittatilaustöitä ja suurempia kokonaisuuksia. Näistä lisää alempana kohdassa projektit.</p>
             </div>
             <div className="articles">
                 <p className="reveal">"Jokaisen huonekalun laatu on pehmeä ja luonnollinen. Voit nähdä, kuinka ainutlaatuinen jokainen kappale on, tehden jokaisesta myytävästä tuotteesta erityisen."</p>
-                <div className="article-images reveal">
+                <div className="article-images">
                     <div className="article">
                     <p>Elle decoration</p>
                     <img src= {process.env.PUBLIC_URL + "/images/artikkeli1.jpeg"} alt="Elle decoration"/>
